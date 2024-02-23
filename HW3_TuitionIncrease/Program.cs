822646a8503e3749f894f739a4d0b35987186e5c
Aditi Mahangade <Aditi.A.Mahangade-1@ou.edu>
2024-02-23 11:11:38 -0600

    Add project files.

---
+++ HW3_TuitionIncrease/Program.cs
@@ -0,0 +1,13 @@
+﻿// Aditi Mahangade
+// HW3 Task 3 Tuition Increase
+
+double tuitionAmount = 12000.0;
+
+int N = 7;
+
+for (int i = 0; i < N; i++)
+{
+    tuitionAmount = tuitionAmount * (1 + 0.05);
+    string message = string.Format($"The tuition after year {i + 1} will be {tuitionAmount:C0}.");
+    Console.WriteLine( message );
+}
